9308c699855121d45d0b3c7719dd307e6c1dcc7f
EJ Gordon <ejgordoncode@gmail.com>
2025-10-31 23:22:04 -0700

    Create pass2.cpp and add pseudocode
    The comments outline the steps that need to be accomplished in pass2.

---
+++ pass2.cpp
@@ -0,0 +1,9 @@
+// read intermediate file and load SYMTAB and OPTAB to be used
+
+// translate instructions to object code (do not produce .obj file)
+
+// handle pc and base-relative addressing
+
+//format output for .l file (see fig 2.10 in book)
+
+//output .l and .st files
